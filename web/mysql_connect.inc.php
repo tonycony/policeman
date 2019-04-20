5c6fda119bc94f77e39cea40dc0de933a6c4c67a
tonycony <tony40727@gmail.com>
2019-04-20 11:29:04 +0800

    Create mysql_connect.inc.php

---
+++ web/mysql_connect.inc.php
@@ -0,0 +1,24 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+//資料庫設定
+//資料庫位置
+$db_server = "54.156.124.2";
+//資料庫名稱
+$db_name = "linebot";
+//資料庫管理者帳號
+$db_user = "case";
+//資料庫管理者密碼
+$db_passwd = "1234";
+
+//對資料庫連線
+$link=mysqli_connect($db_server, $db_user, $db_passwd)
+       or die("無法對資料庫連線");
+
+//資料庫連線採UTF8
+mysqli_query($link,'SET CHARACTER SET utf8');
+mysqli_query($link,"SET collation_connection = 'utf8_general_ci'");
+
+//選擇資料庫
+mysqli_select_db($link,$db_name)
+      or die("無法使用資料庫");
+?>  
