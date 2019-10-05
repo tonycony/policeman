6378b5736edfaa396199cb2541bfb3c3e98831b8
tonycony <46638919+tonycony@users.noreply.github.com>
2019-10-06 03:07:28 +0800

    Update mysql_connect.inc.php

--- web/mysql_connect.inc.php
+++ web/mysql_connect.inc.php
@@ -12,8 +12,8 @@ $db_passwd = "1234";
 $link=mysqli_connect($db_server, $db_user, $db_passwd)
        or die("無法對資料庫連線");
 //資料庫連線採UTF8
-mysqli_query($link,'SET CHARACTER SET utf8');
-mysqli_query($link,"SET collation_connection = 'utf8_general_ci'");
+//mysqli_query($link,'SET CHARACTER SET utf8');
+//mysqli_query($link,"SET collation_connection = 'utf8_general_ci'");
 //選擇資料庫
 mysqli_select_db($link,$db_name)
       or die("無法使用資料庫");
