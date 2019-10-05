9518d8d8cc0294d999cdf61bfc904765958e02f3
tonycony <46638919+tonycony@users.noreply.github.com>
2019-10-06 02:54:29 +0800

    Update mysql_connect.inc.php

--- web/mysql_connect.inc.php
+++ web/mysql_connect.inc.php
@@ -4,10 +4,10 @@
   define("DB_PASSWORD", "1234");
   define("DB_DATABASE", "linebot");
 
-  $db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+  $link = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
   // you could test connection eventually using a if and else conditional statement, 
   // feel free to take out the code below once you see Connected!
-  if ($db) {
+  if ($link) {
     echo "Connected!";
   } else {
     echo "Connection Failed";
